2aa1acc7181292c4a5e9f7638fb593863163bf8c
Stefano De Lorenzo <stefanodl818@gmail.com>
2023-11-30 17:04:10 +0100

    added order placement

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
 import RestaurantScreen from "./screens/RestaurantScreen";
+import BasketScreen from "./screens/BasketScreen";
 import { Provider } from "react-redux";
 import { store } from "./store";
 
@@ -18,6 +19,16 @@ export default function App() {
           <Stack.Navigator>
             <Stack.Screen name="Home" component={HomeScreen} />
             <Stack.Screen name="Restaurant" component={RestaurantScreen} />
+            <Stack.Screen
+              name="Basket"
+              component={BasketScreen}
+              options={{ presentation: "modal", headerShown: false }}
+            />
+            <Stack.Screen
+              name="PreparingOrderScreen"
+              component={PreparingOrderScreen}
+              options={{ presentation: "fullScreenModal", headerShown: false }}
+            />
           </Stack.Navigator>
         </TailwindProvider>
       </Provider>
--- features/basketSlice.js
+++ features/basketSlice.js
@@ -15,7 +15,7 @@ export const basketSlice = createSlice({
       const index = state.items.findIndex(
         (item) => item.id === action.payload.id
       );
-      let newbasket = [...state.items];
+      let newBasket = [...state.items];
       if (index >= 0) {
         newBasket.splice(index, 1);
       } else {
@@ -33,5 +33,8 @@ export const selectBasketItems = (state) => state.basket.items;
 
 export const selectBasketItemsWithId = (state, id) =>
   state.basket.items.filter((item) => item.id == id);
+
+export const selectBasketTotal = (state) =>
+  state.basket.items.reduce((total, item) => (total += item.price), 0);
 
 export default basketSlice.reducer;
